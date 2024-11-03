41be568e856a3ab0a8304f6c68165b7dd8f2253b
Casper Engelmann <me@casperengelmann.com>
2024-11-03 23:20:03 +0100

    Refactor environment configuration to use DATABASE_URL_EXTERNAL and enhance dotenv setup for improved clarity

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,19 +1,11 @@
-import { config } from "dotenv";
 import { defineConfig } from "drizzle-kit";
-import invariant from "invariant";
 import { env } from "~/env";
-
-config({
-  path: ".env.local",
-});
-
-invariant(env.DATABASE_URL, "DATABASE_URL is required");
 
 export default defineConfig({
   dialect: "postgresql",
   schema: "./src/db/schema.ts",
   out: "./drizzle",
   dbCredentials: {
-    url: env.DATABASE_URL,
+    url: env.DATABASE_URL_EXTERNAL,
   },
 });
--- src/env.ts
+++ src/env.ts
@@ -1,10 +1,19 @@
 import { createEnv } from "@t3-oss/env-nextjs";
+import { config } from "dotenv";
+import { expand } from "dotenv-expand";
 import { z } from "zod";
+
+expand(
+  config({
+    path: ".env.local",
+  }),
+);
 
 export const env = createEnv({
   server: {
     AUTH_SECRET: z.string().min(1),
     DATABASE_URL: z.string().url(),
+    DATABASE_URL_EXTERNAL: z.string().url(),
     RESEND_KEY: z.string().min(1),
   },
   client: {
@@ -13,6 +22,7 @@ export const env = createEnv({
   runtimeEnv: {
     AUTH_SECRET: process.env.AUTH_SECRET,
     DATABASE_URL: process.env.DATABASE_URL,
+    DATABASE_URL_EXTERNAL: process.env.DATABASE_URL_EXTERNAL,
     RESEND_KEY: process.env.RESEND_KEY,
     NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
   },
