119a2ddb3dfad8350f8292a71f9a8f7bd10c99d8
Casper Engelmann <me@casperengelmann.com>
2024-11-04 03:41:39 +0100

    Add environment variables to docker-compose and validate APP_URL in env.ts for better configuration and runtime consistency

--- src/env.ts
+++ src/env.ts
@@ -11,17 +11,17 @@ expand(
 
 export const env = createEnv({
   server: {
-    AUTH_SECRET: z.string().min(1),
-    DATABASE_URL: z.string().url(),
-    DATABASE_URL_EXTERNAL: z.string().url(),
-    RESEND_KEY: z.string().min(1),
     APP_URL: z.string().url(),
+    AUTH_SECRET: z.string().min(1),
+    DATABASE_URL_EXTERNAL: z.string().url(),
+    DATABASE_URL: z.string().url(),
+    RESEND_KEY: z.string().min(1),
   },
   runtimeEnv: {
-    AUTH_SECRET: process.env.AUTH_SECRET,
-    DATABASE_URL: process.env.DATABASE_URL,
-    DATABASE_URL_EXTERNAL: process.env.DATABASE_URL_EXTERNAL,
-    RESEND_KEY: process.env.RESEND_KEY,
     APP_URL: process.env.APP_URL,
+    AUTH_SECRET: process.env.AUTH_SECRET,
+    DATABASE_URL_EXTERNAL: process.env.DATABASE_URL_EXTERNAL,
+    DATABASE_URL: process.env.DATABASE_URL,
+    RESEND_KEY: process.env.RESEND_KEY,
   },
 });
