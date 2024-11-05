99e0c8dca6403e153c9f4c50875f7e14981b0e62
Casper Engelmann <me@casperengelmann.com>
2024-11-05 23:28:37 +0100

    Refactor database URL configuration in drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,18 @@
 import { defineConfig } from "drizzle-kit";
 import invariant from "invariant";
 
-invariant(process.env.DATABASE_URL, "DATABASE_URL is required");
+const dbUrl =
+  process.env.NODE_ENV === "production"
+    ? process.env.DATABASE_URL
+    : process.env.DATABASE_URL_EXTERNAL;
+
+invariant(dbUrl, "dbUrl is required");
 
 export default defineConfig({
   dialect: "postgresql",
   schema: "./src/db/schema.ts",
   out: "./drizzle",
   dbCredentials: {
-    url: process.env.DATABASE_URL,
+    url: dbUrl,
   },
 });
