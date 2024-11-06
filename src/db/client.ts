4fe5b6b66e7ef63ef18811b95bf74d5a96b7c56c
Casper Engelmann <me@casperengelmann.com>
2024-11-06 03:00:54 +0100

    Update DATABASE_URL in db/client.ts

--- src/db/client.ts
+++ src/db/client.ts
@@ -3,6 +3,6 @@ import postgres from "postgres";
 import * as schema from "~/db/schema";
 import { env } from "~/env";
 
-export const pool = postgres(env.DATABASE_URL_EXTERNAL, { max: 1 });
+export const pool = postgres(env.DATABASE_URL, { max: 1 });
 
 export const db = drizzle(pool, { schema });
