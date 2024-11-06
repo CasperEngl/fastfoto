36131abf96b1567f8aaca4295d1f20a0c55af5c2
Casper Engelmann <me@casperengelmann.com>
2024-11-06 19:05:23 +0100

    Add pino logger to instrumentation.ts

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,5 +1,7 @@
 export async function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
+    const { default: pino } = await import("pino");
+    const logger = pino();
     const { PerformanceObserver } = await import("perf_hooks");
 
     // Create a performance observer
@@ -8,7 +10,7 @@ export async function register() {
       entries.forEach((entry) => {
         // Log slow operations (> 1s)
         if (entry.duration > 1000) {
-          console.warn(
+          logger.warn(
             `Slow operation detected: ${entry.name} took ${entry.duration}ms`,
           );
         }
