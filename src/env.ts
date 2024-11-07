4a3687943e3169bef6388221dcb8912e4dfcce42
Casper Engelmann <me@casperengelmann.com>
2024-11-07 01:19:14 +0100

    Add AXIOM_DATASET and AXIOM_TOKEN to environment variables

--- src/env.ts
+++ src/env.ts
@@ -30,6 +30,8 @@ export const env = createEnv({
     DATABASE_URL_EXTERNAL: z.string().url(),
     DATABASE_URL: z.string().url(),
     RESEND_KEY: z.string().min(1),
+    AXIOM_DATASET: z.string().min(1),
+    AXIOM_TOKEN: z.string().min(1),
   },
   runtimeEnv: {
     APP_URL: process.env.APP_URL,
@@ -37,5 +39,7 @@ export const env = createEnv({
     DATABASE_URL_EXTERNAL: process.env.DATABASE_URL_EXTERNAL,
     DATABASE_URL: process.env.DATABASE_URL,
     RESEND_KEY: process.env.RESEND_KEY,
+    AXIOM_DATASET: process.env.AXIOM_DATASET,
+    AXIOM_TOKEN: process.env.AXIOM_TOKEN,
   },
 });
