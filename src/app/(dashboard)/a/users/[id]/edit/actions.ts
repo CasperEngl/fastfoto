75bd51362d973dada57a578f0bf461f11adda8ae
Casper Engelmann <me@casperengelmann.com>
2024-11-07 01:24:21 +0100

    Update user information and validate allowed properties

--- src/app/(dashboard)/a/users/[id]/edit/actions.ts
+++ src/app/(dashboard)/a/users/[id]/edit/actions.ts
@@ -1,28 +1,40 @@
 "use server";
 
+import { eq, InferInsertModel } from "drizzle-orm";
 import { revalidatePath } from "next/cache";
-import { eq } from "drizzle-orm";
 import { auth } from "~/auth";
 import { db } from "~/db/client";
 import { Users } from "~/db/schema";
+import { validateAllowedProperties } from "~/lib/validate-allowed-properties";
 import { isAdmin } from "~/role";
 
-export async function updateUser({
-  userId,
-  email,
-  name,
-}: {
-  userId: string;
-  email: string;
-  name: string;
-}) {
+export async function updateUser(
+  options: Partial<InferInsertModel<typeof Users>> & {
+    id: string;
+  },
+) {
   const session = await auth();
 
   if (!isAdmin(session?.user)) {
     throw new Error("Unauthorized");
   }
 
-  await db.update(Users).set({ email, name }).where(eq(Users.id, userId));
+  if (
+    session?.user.id === options.id &&
+    options.userType !== session.user.userType
+  ) {
+    throw new Error("You cannot modify your own user type");
+  }
+
+  const updateData = {
+    email: options.email,
+    name: options.name,
+    userType: options.userType,
+  } satisfies Partial<InferInsertModel<typeof Users>>;
+
+  validateAllowedProperties(options, updateData, ["id"]);
+
+  await db.update(Users).set(updateData).where(eq(Users.id, options.id));
 
   revalidatePath("/a/users");
 }
