f04360b22951a86b72250d511b28541dd4141a32
Casper Engelmann <me@casperengelmann.com>
2024-11-07 01:24:37 +0100

    Add QueryLogger to drizzle client for logging queries

--- src/db/client.ts
+++ src/db/client.ts
@@ -1,13 +1,23 @@
+import { Logger } from "drizzle-orm";
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import * as schema from "~/db/schema";
 import { env } from "~/env";
 
+class QueryLogger implements Logger {
+  logQuery(query: string, params: unknown[]): void {
+    console.debug("===BEGIN_QUERY===");
+    console.debug(query);
+    console.debug(params);
+    console.debug("===END_QUERY===");
+  }
+}
+
 export const pool = postgres(
   process.env.NODE_ENV === "production"
     ? env.DATABASE_URL
     : env.DATABASE_URL_EXTERNAL,
-  { max: 10, debug: true },
+  { max: 1 },
 );
 
-export const db = drizzle(pool, { schema, logger: true });
+export const db = drizzle(pool, { schema, logger: new QueryLogger() });
