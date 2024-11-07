2d853e50f94078506311e0e8d7cd343af93d7aca
Casper Engelmann <me@casperengelmann.com>
2024-11-07 23:36:02 +0100

    Add not-found and page components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,27 @@
-import Link from "next/link";
-import { SignOutButton } from "~/app/sign-out-button";
-import { auth } from "~/auth";
-import { Button } from "~/components/ui/button";
+import { CallToAction } from "~/app/components/CallToAction";
+import { Faqs } from "~/app/components/Faqs";
+import { Footer } from "~/app/components/Footer";
+import { Header } from "~/app/components/Header";
+import { Hero } from "~/app/components/Hero";
+import { Pricing } from "~/app/components/Pricing";
+import { PrimaryFeatures } from "~/app/components/PrimaryFeatures";
+import { SecondaryFeatures } from "~/app/components/SecondaryFeatures";
+import { Testimonials } from "~/app/components/Testimonials";
 
-export default async function Home() {
-  const session = await auth();
-
+export default function Home() {
   return (
-    <div className="space-y-4">
-      <pre>{JSON.stringify(session, null, 2)}</pre>
-      {session ? (
-        <SignOutButton />
-      ) : (
-        <Button asChild>
-          <Link href="/login">Login</Link>
-        </Button>
-      )}
-    </div>
+    <>
+      <Header />
+      <main>
+        <Hero />
+        <PrimaryFeatures />
+        <SecondaryFeatures />
+        <CallToAction />
+        <Testimonials />
+        <Pricing />
+        <Faqs />
+      </main>
+      <Footer />
+    </>
   );
 }
