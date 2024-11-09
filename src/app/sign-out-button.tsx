eaa52d3e32074f2657ccebe9965332efa2a5b06a
Casper Engelmann <me@casperengelmann.com>
2024-11-09 03:36:58 +0100

    Refactor sign out button functionality

--- src/app/sign-out-button.tsx
+++ src/app/sign-out-button.tsx
@@ -1,16 +1,22 @@
 "use client";
 
 import { signOut } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useActionState } from "react";
 import { Button } from "~/components/ui/button";
 
 export function SignOutButton() {
+  const router = useRouter();
+  const [, action, isPending] = useActionState(async () => {
+    await signOut();
+    await router.push("/login");
+  }, null);
+
   return (
-    <form
-      action={async () => {
-        await signOut({ redirectTo: "/login" });
-      }}
-    >
-      <Button>Sign out</Button>
+    <form action={action}>
+      <Button type="submit" disabled={isPending}>
+        {isPending ? "Signing out..." : "Sign out"}
+      </Button>
     </form>
   );
 }
