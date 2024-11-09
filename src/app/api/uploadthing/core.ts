988f88e64aeb9182e76b0c8a5ed11c828008d9da
Casper Engelmann <me@casperengelmann.com>
2024-11-09 05:23:43 +0100

    Enhance upload functionality: integrate session validation, refactor schema imports, and streamline team logo updates logic

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,11 +1,12 @@
 import crypto from "crypto";
 import { eq } from "drizzle-orm";
+import invariant from "invariant";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError, UTApi } from "uploadthing/server";
 import { z } from "zod";
 import { auth } from "~/auth";
 import { db } from "~/db/client";
-import { Photos, Users } from "~/db/schema";
+import * as schema from "~/db/schema";
 
 const f = createUploadthing();
 
@@ -32,7 +33,7 @@ export const ourFileRouter = {
       }
 
       await db
-        .insert(Photos)
+        .insert(schema.Photos)
         .values({
           id: crypto.randomUUID(),
           albumId: albumId,
@@ -57,12 +58,55 @@ export const ourFileRouter = {
     })
     .onUploadComplete(async ({ metadata, file }) => {
       await db
-        .update(Users)
+        .update(schema.Users)
         .set({
           image: file.url,
           updatedAt: new Date(),
         })
-        .where(eq(Users.id, metadata.userId))
+        .where(eq(schema.Users.id, metadata.userId))
+        .execute();
+
+      return { url: file.url };
+    }),
+  teamLogo: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } })
+    .input(
+      z.object({
+        teamId: z.string().uuid(),
+      }),
+    )
+    .middleware(async ({ input }) => {
+      const session = await auth();
+
+      if (!session?.user?.id) {
+        throw new UploadThingError("Unauthorized");
+      }
+
+      const teamMember = await db.query.TeamMembers.findFirst({
+        where: (teamMembers, { and, eq }) => {
+          invariant(session?.user?.id, "Not authenticated");
+
+          return and(
+            eq(teamMembers.teamId, input.teamId),
+            eq(teamMembers.userId, session.user.id),
+            eq(teamMembers.role, "owner"),
+          );
+        },
+      });
+
+      if (!teamMember) {
+        throw new UploadThingError("Unauthorized - Must be team owner");
+      }
+
+      return { userId: session.user.id, teamId: input.teamId };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      await db
+        .update(schema.Teams)
+        .set({
+          logo: file.url,
+          updatedAt: new Date(),
+        })
+        .where(eq(schema.Teams.id, metadata.teamId))
         .execute();
 
       return { url: file.url };
