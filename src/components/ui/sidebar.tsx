a561c81ee96789a631680bc13a86f8030756e286
Casper Engelmann <me@casperengelmann.com>
2024-11-11 03:57:34 +0100

    Enhance SidebarInset: Add dynamic offset based on sidebar state for improved responsiveness and visual consistency

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -327,12 +327,24 @@ const SidebarInset = React.forwardRef<
   HTMLDivElement,
   React.ComponentProps<"main">
 >(({ className, ...props }, ref) => {
+  const sidebar = useSidebar();
+  const offset =
+    sidebar.state === "collapsed" ? SIDEBAR_WIDTH_ICON : SIDEBAR_WIDTH;
+  const isMobile = useBreakpoint();
+
   return (
     <main
       ref={ref}
+      style={{
+        // @ts-expect-error offset is not typesafe
+        "--offset": `${offset}`,
+      }}
       className={cn(
         "relative flex min-h-svh flex-1 flex-col bg-background",
         "peer-data-[variant=inset]:min-h-[calc(100svh-theme(spacing.4))] md:peer-data-[variant=inset]:m-2 md:peer-data-[state=collapsed]:peer-data-[variant=inset]:ml-2 md:peer-data-[variant=inset]:ml-0 md:peer-data-[variant=inset]:rounded-xl md:peer-data-[variant=inset]:shadow",
+        !isMobile
+          ? "max-w-[calc(100svw-var(--offset)-theme(spacing.10))]"
+          : null,
         className,
       )}
       {...props}
