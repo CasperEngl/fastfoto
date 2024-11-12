c2c9c54118ad1d866f4da78dfa4183e3335bacdc
Casper Engelmann <me@casperengelmann.com>
2024-11-12 23:14:20 +0100

    Remove unused import of env in uploadthing route handler to clean up the code

--- src/app/api/uploadthing/route.ts
+++ src/app/api/uploadthing/route.ts
@@ -1,6 +1,5 @@
 import { createRouteHandler } from "uploadthing/next";
 import { ourFileRouter } from "~/app/api/uploadthing/core";
-import { env } from "~/env";
 
 export const { GET, POST } = createRouteHandler({
   router: ourFileRouter,
