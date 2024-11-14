09e2da721b863b3b28e825091a99667a95f5f568
Casper Engelmann <me@casperengelmann.com>
2024-11-14 09:57:25 +0100

    Refactor OnboardingPage layout and styles for enhanced responsiveness and updated design consistency in text and container sizing

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -1,14 +1,14 @@
-import OnboardingForm from "./onboarding-form";
+import OnboardingForm from "~/app/onboarding/onboarding-form";
 
 export default function OnboardingPage() {
   return (
     <main>
-      <div className="container max-w-3xl py-16">
-        <div className="mb-8">
-          <h1 className="mb-3 text-4xl font-bold tracking-tight text-primary sm:text-5xl">
+      <div className="container max-w-4xl px-4 py-16">
+        <div className="mb-8 px-8">
+          <h1 className="mb-3 text-balance text-4xl font-bold tracking-tight text-primary sm:text-5xl">
             Tell us about your studio
           </h1>
-          <p className="text-lg text-muted-foreground">
+          <p className="text-pretty text-lg text-muted-foreground">
             Let's get your studio set up so you can start managing your business
             more effectively.
           </p>
