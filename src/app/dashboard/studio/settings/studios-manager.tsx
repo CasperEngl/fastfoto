f072475645dfeff95b86a0f59513f828e5d87470
Casper Engelmann <me@casperengelmann.com>
2024-11-18 03:42:02 +0100

    Add support for displaying a "Personal" badge in StudiosManager

--- src/app/dashboard/studio/settings/studios-manager.tsx
+++ src/app/dashboard/studio/settings/studios-manager.tsx
@@ -3,11 +3,13 @@
 import { InferSelectModel } from "drizzle-orm";
 import { motion } from "framer-motion";
 import { ChevronRight } from "lucide-react";
+import { useSession } from "next-auth/react";
 import { parseAsString, useQueryState } from "nuqs";
 import { StudioSettingsForm } from "~/app/dashboard/studio/settings/studio-settings-form";
 import { useContentBreakpoint } from "~/app/hooks/use-content-breakpoint";
 import { Alert, AlertDescription } from "~/components/ui/alert";
 import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
+import { Badge } from "~/components/ui/badge";
 import {
   Select,
   SelectContent,
@@ -35,13 +37,28 @@ export type ManagedStudio = InferSelectModel<typeof schema.Studios> & {
 };
 
 export function StudiosManager({ studios }: { studios: Array<ManagedStudio> }) {
+  const session = useSession();
   const [studioId, setStudioId] = useQueryState(
     "studio",
     parseAsString.withDefault(studios[0]?.id ?? ""),
   );
   const selectedStudio =
     studios.find((studio) => studio.id === studioId) ?? studios[0];
-  const isMobile = useContentBreakpoint("md");
+  const isMobile = useContentBreakpoint("md", {
+    direction: "down",
+  });
+
+  const isPersonalStudio = (studio: ManagedStudio) =>
+    studio.users.some((member) => {
+      if (!session.data?.user?.id) {
+        return false;
+      }
+
+      const isUser = member.id === session.data.user.id;
+      const isOwner = member.role === "owner";
+
+      return isUser && isOwner;
+    });
 
   return (
     <div className="flex flex-col items-start gap-4">
@@ -58,6 +75,14 @@ export function StudiosManager({ studios }: { studios: Array<ManagedStudio> }) {
             {studios.map((studio) => (
               <SelectItem key={studio.id} value={studio.id}>
                 <div>
+                  {isPersonalStudio(studio) ? (
+                    <div>
+                      <Badge variant="secondary" className="-ml-0.5">
+                        Personal
+                      </Badge>
+                    </div>
+                  ) : null}
+
                   <div>{studio.name}</div>
                   <div className="text-xs text-muted-foreground">
                     {pluralize.select(studio.users.length) === "one"
@@ -96,6 +121,14 @@ export function StudiosManager({ studios }: { studios: Array<ManagedStudio> }) {
                         </Avatar>
 
                         <div className="flex-1">
+                          {isPersonalStudio(studio) ? (
+                            <div>
+                              <Badge variant="secondary" className="-ml-0.5">
+                                Personal
+                              </Badge>
+                            </div>
+                          ) : null}
+
                           <div className="truncate text-base text-foreground">
                             {studio.name}
                           </div>
